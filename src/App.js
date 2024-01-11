822d83099d7cfb95046ebb4dd983f0c86729d044
Rk0877 <ramkrishnamahapatro@gmail.com>
2024-01-11 12:31:54 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
+/* eslint-disable no-unused-expressions */
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 function App() {
+  const [products, setproducts] = useState([])
+  const url = 'https://s3.amazonaws.com/open-to-cors/assignment.json';
+
+fetch(url)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network Issue');
+    }
+    return response.json();
+  })
+  .then(data => {
+    setproducts(Object.values(data.products).sort((a,b)=>{
+      return b.popularity - a.popularity
+    }))
+  })
+  .catch(error => {
+    console.error('Fetch error:', error);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Price</th>
+          <th>Popularity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {products.map(product => (
+          <tr key={product.id}>
+            <td>{product?.title}</td>
+            <td>${product?.price}</td>
+            <td>{product?.popularity}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
     </div>
   );
 }
